79d2fd4d5350294d04a223486b46c089ca948ce8
Ian Liu <ian.liu@conversica.com>
2022-08-03 17:00:11 -0700

    add fast food

---
+++ FastFood/OrderSystem.java
@@ -0,0 +1,12 @@
+package FastFood;
+
+public class OrderSystem {
+  public static void main(String[] args) {
+    // intialize menu from menu.json
+    // take in json order
+    // display orders and total
+
+    // take in xml order
+    // display orders and total
+  }
+}
